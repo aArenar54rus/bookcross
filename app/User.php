6a9fa8cd28d1209f3bda4efa1b9c693b445843d9
Станислав Семибоков < >
2016-08-18 16:41:43 +0700

    Add new functions (roles and hasRole) in User.php

--- app/User.php
+++ app/User.php
@@ -25,4 +25,18 @@ class User extends Authenticatable
     {
         return $this->hasMany('App\Posts','author_id');
     }
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
+
+    public function hasRole($role)
+    {
+        if (is_string($role))
+        {
+            return $this->roles->contains('name', $role);
+        }
+        return !! $role->intersect($this->roles)->count();
+    }
 }
