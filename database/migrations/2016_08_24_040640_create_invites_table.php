0e554a24834cc34b8b7f19b39e6f781e1859c6cc
Станислав Семибоков < >
2016-08-24 11:23:54 +0700

    Create migration invites

---
+++ database/migrations/2016_08_24_040640_create_invites_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInvitesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invites',function (Blueprint $table){
+            $table->increments('id');
+            $table->integer('inviter_id');
+            $table->integer('invitee_id');
+            $table->string('code');
+            $table->string('email');
+            $table->timestamp('claimed')->nullable();
+            $table->timestamp('date_expired')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('invites');
+    }
+}
